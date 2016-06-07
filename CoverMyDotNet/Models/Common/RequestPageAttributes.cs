8f9aba11a104e9515dccdc60597307830fb1f329
Chris DeRosa <crobject@users.noreply.github.com>
2016-06-07 16:35:07 -0400

    Update RequestPageAttributes.cs

--- CoverMyDotNet/Models/Common/RequestPageAttributes.cs
+++ CoverMyDotNet/Models/Common/RequestPageAttributes.cs
@@ -5,7 +5,7 @@ namespace CoverMyDotNet
 {
     public class RequestPageAttributes
     {
-		public List<FormQuestionAttributes> Forms { get; set; }
+	    public List<FormQuestionAttributes> Forms { get; set; }
 	    public List<ActionAttributes> Actions { get; set; }
 	    public List<ProvidedCodedReferenceAttributes> ProvidedCodedReferences { get; set; }
 	    public List<ValidationAttributes> Validations { get; set; }
